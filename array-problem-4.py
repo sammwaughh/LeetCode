a0006f5b954937471748c85385af91857cbffd27
Samuel Waugh <samjmwaugh@gmail.com>
2022-10-16 20:53:58 +0100

    tried quadratic approach

--- array-problem-4.py
+++ array-problem-4.py
@@ -1,4 +1,28 @@
 # Contains Duplicate
 
 def containsDuplicate(nums):
-    
+    uniqs = []
+    for x in nums:
+        if x in uniqs:
+            return True
+        else:
+            uniqs.append(x)
+    return False
+
+#print(containsDuplicate([1,2,3,1]))
+
+# Above works but is too slow
+
+def containsDuplicate2(nums):
+    i = 0
+    L = len(nums)
+    while i < L-1:
+        j = i + 1
+        while j < L:
+            if nums[i] == nums[j]:
+                return True
+            j += 1
+        i += 1
+    return False
+
+print(containsDuplicate2([]))
