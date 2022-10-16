1a62e1f9c58c93311acbe9d052cf6538dfb4c81f
Samuel Waugh <samjmwaugh@gmail.com>
2022-10-16 20:13:26 +0100

    done

--- array-problem-2.py
+++ array-problem-2.py
@@ -1,4 +1,11 @@
 # Best Time to Buy and Sell Stock II
 
 def maxProfit(prices):
-    return 0
+    total = 0
+    for i in range(len(prices)-1):
+        diff = prices[i+1] - prices[i]
+        if diff > 0:
+            total += diff
+    return total
+
+print(maxProfit([7,1,5,3,6,4]))
