a9a2a40330dfb4d93ba6169f7561fcdbc391f092
Samuel Waugh <samjmwaugh@gmail.com>
2022-10-20 21:40:27 +0100

    added merge sort to problem 4

--- array-problem-4.py
+++ array-problem-4.py
@@ -29,3 +29,25 @@ print(containsDuplicate2([]))
 
 ## This is quadratic
 ## Need to sort in nlogn time then check in linear time
+
+def mergeSort(arr):
+    if len(arr) == 1 or len(arr) == 0:
+        return arr
+    pivot = arr[0]
+    a1 = []
+    a2 = []
+    for ele in arr[1:]:
+        if ele < pivot:
+            a1.append(ele)
+        else:
+            a2.append(ele)
+    return mergeSort(a1) + [pivot] + mergeSort(a2)
+
+def containsDuplicate3(nums):
+    nums = mergeSort(nums)
+    for i in range(len(nums)-1):
+        if nums[i] == nums[i+1]:
+            return True
+    return False
+
+print(set([1,3,1,4,4]))
