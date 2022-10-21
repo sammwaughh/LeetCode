f943f3c9d4fce3caed67f7165ea311aea539a409
Samuel Waugh <samjmwaugh@gmail.com>
2022-10-21 16:20:32 +0100

    quadratic approach

--- array-problem-6.py
+++ array-problem-6.py
@@ -3,4 +3,11 @@
 # Return an array of the intersection of the two arrays
 
 def intersect(nums1, nums2):
-    return 0
+    intersection = []
+    for ele in nums1:
+        if ele in nums2:
+            intersection.append(ele)
+            nums2.remove(ele)
+    return intersection
+
+print(intersect([4,9,5], [9,4,9,8,4]))
