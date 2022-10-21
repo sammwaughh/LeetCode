05d91beeb7bf6cc81856f05ea497a748c33d78d8
Samuel Waugh <samjmwaugh@gmail.com>
2022-10-21 16:16:02 +0100

    done

--- array-problem-5.py
+++ array-problem-5.py
@@ -1,4 +1,13 @@
 # Single Number
 
 def singleNumber(nums):
-    
+    seen = []
+    for x in nums:
+        if x not in seen:
+            seen.append(x)
+        else:
+            seen.remove(x)
+    return seen[0]
+
+arr = [1,4,1,2,4]
+print(singleNumber(arr))
